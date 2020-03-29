93595a4798a7f4cd3fbffab5675bacc5193615ea
jiri.pavlik <jiri.pavlik@mycroftmind.com>
2020-03-29 17:08:14 +0200

    Add server.js main file with express server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}`);
+});
