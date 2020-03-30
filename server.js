cbbbb187793bb640692d92350904474f07ad401f
jiri.pavlik <jiri.pavlik@mycroftmind.com>
2020-03-30 08:42:15 +0200

    Add implementation of stripe payment

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripe = require('stripe')(stripeSecretKey)
 
 const app = express();
+app.use(express.json())
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -20,6 +22,44 @@ app.get('/store', (req, res) => {
         }
 
         res.render('store.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: JSON.parse(data)
+        })
+
+    })
+})
+
+app.post('/purchase', (req, res) => {
+    fs.readFile('items.json', (err, data) => {
+        if (err) {
+            res.status(500).end();
+        }
+
+        const itemsJson = JSON.parse(data)
+        const itemsArray = itemsJson.music.concat(itemsJson.merch)
+
+        let total = 0
+
+        req.body.items.forEach((item) => {
+            const itemJson = itemsArray.find( (i) => i.id == item.id)
+            total = total + itemJson.price * item.quantity
+        })
+
+        stripe.charges.create({
+            amount: total,
+            source: req.body.stripeTokenId,
+            currency: 'usd',
+        }).then(function() {
+                console.log('charge succesful');
+                res.json({message: 'Successfully purchased items'})
+            })
+            .catch(function(err) {
+                console.log('Charge fail')
+                res.status(500).end()
+            })
+
+        res.render('store.ejs', {
+            stripePublicKey: stripePublicKey,
             items: JSON.parse(data)
         })
 
